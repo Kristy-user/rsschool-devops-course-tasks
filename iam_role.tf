aecd9f3637b05918226b40bac08157a0e9795004
Kristina Klepik <kristygomel@gmail.com>
2025-06-12 22:31:09 +0200

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -47,7 +47,7 @@ resource "aws_iam_role_policy" "dynamodb_locking_policy" {
           "dynamodb:GetItem",
           "dynamodb:PutItem",
           "dynamodb:DeleteItem",
-          "dynamodb:DescribeContinuousBackups"   # <--- add this permission
+          "dynamodb:DescribeContinuousBackups"
         ]
         Resource = "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.dynamodb_table_name}"
       }

e6f131b3261d35a4c3ea2c71532c422874fe676d
Kristina Klepik <kristygomel@gmail.com>
2025-06-12 22:43:31 +0200

    Refactored with full access to dynamodb

--- iam_role.tf
+++ iam_role.tf
@@ -33,27 +33,9 @@ resource "aws_iam_role" "github_actions_role" {
   }
 }
 
-resource "aws_iam_role_policy" "dynamodb_locking_policy" {
-  name = "TerraformDynamoDBLockingPolicy"
-  role = aws_iam_role.github_actions_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:DescribeTable",
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:DescribeContinuousBackups",
-          "dynamodb:DescribeTimeToLive"
-        ]
-        Resource = "arn:aws:dynamodb:${var.aws_region}:${var.aws_account_id}:table/${var.dynamodb_table_name}"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "full_dynamodb_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "policy_attachments" {

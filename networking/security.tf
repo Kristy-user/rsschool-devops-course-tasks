5ef3733e2fec20158f0137f6aa3a9e6db81540e9
Kristina Klepik <kristygomel@gmail.com>
2025-06-28 17:09:24 +0200

    Edited with terraform fmt

--- networking/security.tf
+++ networking/security.tf
@@ -36,9 +36,9 @@ resource "aws_security_group" "k3s_nodes_sg" {
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
 
